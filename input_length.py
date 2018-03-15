95e858ac076fd92c9bcd4d0032dbdb32452c7e46
Prathyusha Konda <prathyushakonda81@gmail.com>
2018-03-15 15:08:34 -0300

    Update input_length.py

--- input_length.py
+++ input_length.py
@@ -1,6 +1,6 @@
 
 
 num = raw_input("\nPlease enter which length peptides you want to predict; if multiple lengths, seperate numbers with space 8 9 10 11: ")
-	input_list = num.split()
-	input_list = [int(a) for a in input_list]
-  return input_list
+input_list = num.split()
+input_list = [int(a) for a in input_list]
+return input_list
